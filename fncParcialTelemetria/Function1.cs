4c6c257bb9a37c3e290a022ac0d5d2e885056da1
Matias Torrico <a2016114304@estudiantes.upsa.edu.bo>
2021-04-20 20:13:36 -0400

    Funcion creado y funcional

--- fncParcialTelemetria/Function1.cs
+++ fncParcialTelemetria/Function1.cs
@@ -1,16 +1,31 @@
 using System;
+using System.Threading.Tasks;
+using fncParcialTelemetria.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace fncParcialTelemetria
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([ServiceBusTrigger("MyConn", Connection = "")]string myQueueItem, ILogger log)
+        public static async Task RunAsync(
+            [ServiceBusTrigger("mdtm-cola", Connection = "MyConn")] string myQueueItem,
+            [CosmosDB(databaseName: "Telemetria", collectionName: "Datos", ConnectionStringSetting = "strCosmos")] IAsyncCollector<object> datos,
+            ILogger log)
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            try
+            {
+                log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+                var data = JsonConvert.DeserializeObject<Data>(myQueueItem);
+                await datos.AddAsync(data);
+            }
+            catch (Exception e)
+            {
+                log.LogError($"No fue posible insertar datos: {e.Message}");
+            }
         }
     }
 }
---
+++ fncParcialTelemetria/Models/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace fncParcialTelemetria.Models
+{
+    class Data
+    {
+        [Key]
+        public string id { get; set; }
+
+        [Required]
+        public string Estudiante { get; set; }
+
+        [Required]
+        public string Temperatura { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime Fecha { get; set; }
+
+        [Required]
+        public string Hora { get; set; }
+    }
+}
